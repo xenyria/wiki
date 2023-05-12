78a25a716b2fbeebe7f9ecfbee10d17d0a247630
OfficialCRUGG <ich@crugg.de>
2023-05-12 19:26:54 +0200

    fix: Update Message Link

--- src/config.tsx
+++ src/config.tsx
@@ -9,7 +9,10 @@ const config: Config = {
       <span>
         Welcome to the Xenyria Wiki! This is a work in progress, so please be
         patient as we work on it. Read more{" "}
-        <Link href="/general/about-this-wiki">here</Link>.
+        <Link href="/meta" className="underline">
+          here
+        </Link>
+        .
       </span>
     ),
     key: "welcome_to_wiki",
